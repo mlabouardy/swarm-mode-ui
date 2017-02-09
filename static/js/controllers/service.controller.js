b3ad224aa628571eb2bde4e62275f13a0ae020ec
mlabouardy <mohamed@labouardy.com>
2017-02-09 21:14:21 +0100

    show service details

--- static/js/controllers/service.controller.js
+++ static/js/controllers/service.controller.js
@@ -1,6 +1,7 @@
 var ServiceCtrl = function($routeParams, $scope, SwarmService){
     SwarmService.inspectService($routeParams.id).then(function(results){
         $scope.info = results.plain();
+        $scope.info.CreatedAt = moment($scope.info.CreatedAt).fromNow();
     });
 };
 
